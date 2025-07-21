7723c5bb8c79fef836d00056964d5835e1891b56
Atharva-Kanerkar <atharvakanherkar25@gmail.com>
2025-07-22 01:39:27 +0530

    Added api

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,6 @@
 // frontend/lib/api.ts
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://redlead.onrender.com/';
 
 /**
  * A helper function to create standardized authentication headers.
